9eb7503ff7626bdea9eb8f9c22109f07d47bd988
yuenwinyun <geek@yuenwinyun.com>
2022-06-07 23:31:32 +0800

    feat: add countSubstring

---
+++ lc/countSubstrings.ts
@@ -0,0 +1,29 @@
+export function countSubstrings(s: string): number {
+	let count = 0;
+
+	for (let i = 0; i < s.length; i++) {
+		let l = i,
+			r = i;
+		// even
+		while (s[l] === s[r] && l >= 0 && r < s.length) {
+			count++;
+			l--;
+			r++;
+		}
+
+		l = i;
+		r = i + 1;
+		// odd
+		while (s[l] === s[r] && l >= 0 && r < s.length) {
+			count++;
+			l--;
+			r++;
+		}
+	}
+
+	return count;
+}
+
+let result = countSubstrings("abc");
+
+result;
