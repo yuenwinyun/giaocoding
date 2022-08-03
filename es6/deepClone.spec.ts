0981e13d1ebabf2363dff7248892be1a28dee091
yuenwinyun <geek@yuenwinyun.com>
2022-08-03 09:42:17 +0800

    refactor: refactor

--- es6/deepClone.spec.ts
+++ es6/deepClone.spec.ts
@@ -2,9 +2,12 @@ export function deepClone<T extends {}>(data: T): T {
 	if (typeof data === 'string') {
 		return data;
 	}
+	return data;
 }
 
 function typeOf<T extends {}>(data: T): string {
 	if (typeof data === 'object') {
+		return '';
 	}
+	return '';
 }
---
+++ es6/promise/promise.spec.ts
@@ -0,0 +1,23 @@
+class PromiseImpl<T = unknown> {
+	#state: 'pending' | 'fulfilled' | 'rejected';
+
+	constructor(executor: (resolver: (value: T) => void, reject: (reason: Error) => void) => void) {
+		this.#state = 'pending';
+	}
+
+	#changeState() {
+		if (this.#state !== 'pending') {
+			return;
+		}
+	}
+
+	then() {}
+}
+
+describe('PromiseImpl', () => {
+	it('ok', () => {
+		const promise = new PromiseImpl(() => {});
+		expect(typeof promise).toBe(typeof new Promise(() => {}));
+		expect(promise.then).toEqual(expect.any(Function));
+	});
+});
